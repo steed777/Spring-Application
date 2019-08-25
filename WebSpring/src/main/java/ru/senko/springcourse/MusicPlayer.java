74f46abe20122becc3213f20fe00c524f337b65e
steed777 <steed777@yandex.ru>
2019-08-25 23:25:52 +0300

    Next3 Spring application:Внедрение зависимостей Внедрение зависимостей происходит с помощью сетторов, в applicationContex файле, а также с помощью musicPlaeг.properties файла

--- WebSpring/src/main/java/ru/senko/springcourse/MusicPlayer.java
+++ WebSpring/src/main/java/ru/senko/springcourse/MusicPlayer.java
@@ -2,13 +2,33 @@ package ru.senko.springcourse;
 
 public class MusicPlayer {
 private Muzic muzic;
+private String name;
+private int valume;
 
 //IoC patern внедряем зависимость
 public MusicPlayer(Muzic muzic) {
 	
 	this.muzic = muzic;
 }
+public MusicPlayer() {
+	// TODO Auto-generated constructor stub
+}
+public void setMuzic(Muzic muzic) {
+	this.muzic = muzic;
+}
 public void playMusic() {
 	System.out.println("Plaing: " + muzic.getSong());
+}
+public int getValume() {
+	return valume;
+}
+public void setValume(int valume) {
+	this.valume = valume;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
 }
 }
